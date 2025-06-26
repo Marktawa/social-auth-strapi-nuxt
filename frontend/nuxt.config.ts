fb92028adfb18865426b2939b9b2acd32e4a0493
Mark Munyaka <markmunyaka@gmail.com>
2025-06-26 14:14:28 +0000

    Nuxt Configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-    vite: {
+  ssr: false,
+  vite: {
     server: {
       allowedHosts: true,
     },
   },
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: 'http://localhost:1337/api',
+        changeOrigin: true
+      }
+    }
+  }
 })
