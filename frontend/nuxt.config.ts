d86ce55f5fb7706c080860c618d64eeab56ec986
Mark Munyaka <markmunyaka@gmail.com>
2025-06-26 13:23:26 +0000

    Install Nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,14 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['nuxt-icon'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    vite: {
     server: {
       allowedHosts: true,
     },
