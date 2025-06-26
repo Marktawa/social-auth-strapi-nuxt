1ad62713ae2174e710f469fb501b899fb3755771
Mark Munyaka <markmunyaka@gmail.com>
2025-06-26 13:27:41 +0000

    Building the Home page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the NoteApp</h1>
+    <p>Your number one notes sharing application</p>
+    <p>Share your notes with anybody across the globe</p>
+    <NuxtLink to="/login">
+      <button>Login</button>
+    </NuxtLink>
+  </div>
+</template>
